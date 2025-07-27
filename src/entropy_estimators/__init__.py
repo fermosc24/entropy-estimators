7640281a8af15b66a30a8a479fddbea1f2b99cdb
Fermin Moscoso del Prado <fermin@dyn-10-112-19-190-guest.wless.uoregon.edu>
2025-07-26 17:48:46 -0700

    Expose dict_to_ndarray in package __init__.py

--- src/entropy_estimators/__init__.py
+++ src/entropy_estimators/__init__.py
@@ -1,2 +1,3 @@
-from .core import Entropy, FreqShrink, JS_KullbackLeibler, JS_JensenShannon
+from .core import Entropy, FreqShrink, JS_KullbackLeibler, JS_JensenShannon, dict_to_ndarray, sample_frequencies
+
 
